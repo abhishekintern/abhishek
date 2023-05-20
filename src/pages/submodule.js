f403785e768c5f6bfa76ec07132e0e821f012b2a
abhishekintern <abhishekb@letsupgrade.in>
2023-05-20 11:34:13 +0530

    add: timer page added for countdown poc

---
+++ src/pages/submodule.js
@@ -0,0 +1,11 @@
+import SubModule from "@/submodules/submodule1/src/components/SubModule";
+
+const SubModulePage = () => {
+  return (
+    <div>
+      <SubModule />
+    </div>
+  );
+};
+
+export default SubModulePage;
---
+++ src/pages/timer.js
@@ -0,0 +1,12 @@
+import CountdownTimer from "@/components/CountDown";
+import React from "react";
+
+const Timer = () => {
+  return (
+    <div>
+      <CountdownTimer />
+    </div>
+  );
+};
+
+export default Timer;
