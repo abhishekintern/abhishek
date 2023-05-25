cda14f7f23b2d06391717b5d9b574e87c90a9782
abhishekintern <abhishekb@letsupgrade.in>
2023-05-25 12:11:32 +0530

    add: idlejs page added with react-idle-timer

---
+++ src/pages/idlejs.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useIdleTimer } from "react-idle-timer";
+
+export default function IdleJS() {
+  const [state, setState] = useState("Active");
+  const [remaining, setRemaining] = useState(0);
+  const [totalTime, setTotalTime] = useState(0);
+
+  const onIdle = () => {
+    setState("Idle");
+  };
+
+  const onActive = () => {
+    setState("Active");
+  };
+
+  const { getRemainingTime, getTotalIdleTime } = useIdleTimer({
+    onIdle,
+    onActive,
+    timeout: 10_000,
+    throttle: 500,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemaining(Math.ceil(getRemainingTime() / 1000));
+      setTotalTime(Math.ceil(getTotalIdleTime() / 1000));
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center p-5">
+      <div className="h-full w-full border-2 bg-purple-100 flex flex-col items-center justify-center">
+        <h1>React Idle Timer</h1>
+        <br />
+        <p>Current State: {state}</p>
+        <p>{remaining} seconds remaining</p>
+        <p>{totalTime} seconds Idle Time</p>
+      </div>
+    </div>
+  );
+}
